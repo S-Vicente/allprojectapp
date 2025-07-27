9e7e36300d8bd2761fc0ffb8516b4da02816e118
S-Vicente <svicente@bol.com.br>
2025-07-27 13:40:34 -0300

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,8 +1,22 @@
-const CACHE_NAME = "allprojectapp-v1";
-const ASSETS = [ ".", "index.html", "style.css", "script.js", "manifest.json" ];
+cconst CACHE_NAME = "allprojectapp-v1";
+const ASSETS = [
+  "/",
+  "/index.html",
+  "/style.css",
+  "/script.js",
+  "/manifest.json",
+  "/service-worker.js"
+];
+
 self.addEventListener("install", e =>
-  e.waitUntil(caches.open(CACHE_NAME).then(c => c.addAll(ASSETS)))
+  e.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(cache => cache.addAll(ASSETS))
+  )
 );
+
 self.addEventListener("fetch", e =>
-  e.respondWith(caches.match(e.request).then(r => r || fetch(e.request)))
+  e.respondWith(
+    caches.match(e.request).then(r => r || fetch(e.request))
+  )
 );
